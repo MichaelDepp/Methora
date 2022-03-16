83a564bdae0aafc9403651acbfa9a028d60152a3
MichaelDepp <michaeldepp7@gmail.com>
2022-03-16 19:17:17 +0800

    feat: add the progress bar and fix the handling for headertitle

--- src/screens/Skills/index.jsx
+++ src/screens/Skills/index.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import ProfileBlob from 'components/ProfileBlob';
 import Container from '../../components/Container';
 import HeaderTitle from '../../components/HeaderTitle';
+import ProgressBar from '../../components/ProgressBar';
 
 const Skills = () => {
     return (
         <Container>
             <div className="pt-8">
-                <HeaderTitle />
+                <HeaderTitle title={'Skills'} />
             </div>
             <div className="pt-28 lg:pt-80 flex justify-center">
                 <ProfileBlob image={'./assets/memoji-skills.webp'} />
+            </div>
+            <div className="grid justify-items-center mt-4">
+                <ProgressBar skillName={'Flutter'} percentage={100} />
+                <ProgressBar skillName={'React Native'} percentage={90} />
+                <ProgressBar skillName={'Machine Learning'} percentage={45} />
+                <ProgressBar skillName={'Python'} percentage={85} />
+                <ProgressBar skillName={'Ui Design'} percentage={90} />
             </div>
         </Container>
     );
