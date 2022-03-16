b087d456040b9d535dbbce7ab7bd8521114bb42f
MichaelDepp <michaeldepp7@gmail.com>
2022-03-16 19:16:33 +0800

    refactor: make headertitle dynamic

--- src/components/HeaderTitle/index.jsx
+++ src/components/HeaderTitle/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const HeaderTitle = () => {
+const HeaderTitle = ({ title }) => {
     return (
         <div className="relative">
             <h1 className="absolute right-0 left-0 top-2.5 font-bold text-3xl text-theme-primary text-center z-10">
-                Skills
+                {title}
             </h1>
-            <span className="absolute inset-0 text-6xl font-bold leading-none text-center text-theme-secondary">
-                Skills
+            <span className="absolute inset-0 text-6xl font-bold leading-none text-center text-theme-bg/5 dark:text-theme-lightBg/5">
+                {title}
             </span>
         </div>
     );
