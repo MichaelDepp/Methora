aab50ba7cd2fbc2ea537776a5f2872914941bcf2
MichaelDepp <michaeldepp7@gmail.com>
2022-03-16 15:02:07 +0800

    feat: add button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Button = ({ text, onClick }) => {
+    if (!text) {
+        return null;
+    }
+    return (
+        <button
+            onClick={onClick}
+            className="text-white dark:text-theme-bg bg-theme-primary focus:ring-4 font-bold rounded-lg text-lg px-5 py-2 text-center mb-2">
+            {text}
+        </button>
+    );
+};
+
+export default Button;
