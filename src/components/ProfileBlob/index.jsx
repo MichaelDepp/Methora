051d7ef92db0beb670cbb09ae40aabfa97b478c3
MichaelDepp <michaeldepp7@gmail.com>
2022-03-16 11:22:31 +0800

    feat: adjust the blob to be responsive

--- src/components/ProfileBlob/index.jsx
+++ src/components/ProfileBlob/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ProfileBlob = (props) => {
     return (
-        <div className="bg-theme-primary rounded-full">
+        <div className="bg-theme-primary w-64 h-64 lg:w-96 lg:h-96 rounded-full animate-blob shadow-2xl">
             <img
-                className="object-contain w-64 h-64"
+                className="object-contain w-64 h-64 lg:w-96 lg:h-96"
                 src={props.image}
                 // src="https://randomuser.me/api/portraits/women/81.jpg"
                 alt="user image"
