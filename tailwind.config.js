cbe3b4c19538baa314482a604d411aac44f8e9ad
MichaelDepp <michaeldepp7@gmail.com>
2022-03-17 19:57:59 +0800

    refactor: use the constant color values in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const constants = require('./src/constants');
+
 module.exports = {
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -64,47 +66,7 @@ module.exports = {
             animation: {
                 blob: 'transform 20s ease-in-out infinite both alternate, bounce 10s infinite both alternate, wiggle 20s ease-in-out infinite'
             },
-            colors: {
-                theme: {
-                    primary: '#835ecf',
-                    secondary: '#262626',
-                    bg: '#1F1F1F',
-                    lightBg: '#fefefe'
-                },
-                gray: {
-                    100: '#f7fafc',
-                    200: '#edf2f7',
-                    300: '#e2e8f0',
-                    400: '#cbd5e0',
-                    500: '#a0aec0',
-                    600: '#718096',
-                    700: '#4a5568',
-                    800: '#2d3748',
-                    900: '#1a202c'
-                },
-                black: {
-                    100: '#000000',
-                    200: '#edf2f7',
-                    300: '#e2e8f0',
-                    400: '#cbd5e0',
-                    500: '#a0aec0',
-                    600: '#718096',
-                    700: '#4a5568',
-                    800: '#2d3748',
-                    900: '#1a202c'
-                },
-                blue: {
-                    100: '#ebf8ff',
-                    200: '#bee3f8',
-                    300: '#90cdf4',
-                    400: '#63b3ed',
-                    500: '#4299e1',
-                    600: '#3182ce',
-                    700: '#2b6cb0',
-                    800: '#2c5282',
-                    900: '#2a4365'
-                }
-            }
+            colors: constants.colors
         }
     },
     plugins: []
