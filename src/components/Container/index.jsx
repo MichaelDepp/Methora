e57316bef66d4c280c096fc09cfee398f7c4e5d8
MichaelDepp <michaeldepp7@gmail.com>
2022-03-19 13:25:41 +0800

    feat: add id as prop in container

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Container = ({ children, fullScreen }) => {
+const Container = ({ children, fullScreen, id }) => {
     return (
         <div
             className={`px-8 bg-theme-lightBg dark:bg-theme-bg ${
                 fullScreen ? 'h-screen' : ''
-            } w-full`}>
+            } w-full`}
+            id={id}>
             {children}
         </div>
     );
