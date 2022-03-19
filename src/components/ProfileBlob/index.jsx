ebdb3f49bc5f981ea7700a796fd33921a8f72556
MichaelDepp <michaeldepp7@gmail.com>
2022-03-20 01:57:10 +0800

    refactor: handle the image round

--- src/components/ProfileBlob/index.jsx
+++ src/components/ProfileBlob/index.jsx
@@ -4,7 +4,7 @@ const ProfileBlob = (props) => {
     return (
         <div className="bg-theme-primary w-64 h-64 lg:w-96 lg:h-96 rounded-full animate-blob shadow-2xl">
             <img
-                className="object-contain w-64 h-64 lg:w-96 lg:h-96"
+                className="object-contain w-64 h-64 lg:w-96 lg:h-96 rounded-full"
                 src={props.image}
                 // src="https://randomuser.me/api/portraits/women/81.jpg"
                 alt="user image"
