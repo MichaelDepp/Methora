b5b83d61e212c6a962ac4c72cb166602f02826d5
MichaelDepp <michaeldepp7@gmail.com>
2022-03-19 15:34:19 +0800

    feat: created chip component

---
+++ src/components/Chip/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Chip = (props) => {
+    const colorConfig = props.selected ? 'bg-theme-primary' : 'bg-gray-200 dark:bg-theme-dark';
+
+    const chipClicked = () => props.onClick(props.name);
+
+    return (
+        <div
+            className={`cursor-pointer flex justify-center items-center m-1 px-4 py-2 rounded-full hover:bg-theme-primary dark:hover:bg-theme-primary ${colorConfig}`}
+            onClick={chipClicked}>
+            <div className={`text-xs font-normal leading-none ${props.selected && 'text-white'}`}>
+                {props.name}
+            </div>
+        </div>
+    );
+};
+
+export default Chip;
