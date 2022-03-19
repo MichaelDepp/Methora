cb21c6f30cb84deda8177b5e2675692f83fa985a
MichaelDepp <michaeldepp7@gmail.com>
2022-03-19 15:35:02 +0800

    feat: add the portfolio chip component to portfolio

--- src/screens/Portfolio/index.jsx
+++ src/screens/Portfolio/index.jsx
@@ -3,6 +3,7 @@ import ProfileBlob from 'components/ProfileBlob';
 import Container from '../../components/Container';
 import HeaderTitle from '../../components/HeaderTitle';
 import PortfolioCard from '../../components/PortfolioCard';
+import PortfolioChip from '../../components/PortfolioChip';
 
 const Portfolio = (props) => {
     return (
@@ -10,7 +11,10 @@ const Portfolio = (props) => {
             <div className="pt-8">
                 <HeaderTitle title={'Portfolio'} />
             </div>
-            <div className="pt-28 justify-items-center grid grid-rows-1 gap-4">
+            <div className="pt-20">
+                <PortfolioChip />
+            </div>
+            <div className="py-12 justify-items-center grid grid-rows-1 gap-4">
                 <PortfolioCard
                     imageSrc={
                         'https://filmfare.wwmindia.com/content/2021/apr/thalaajith11617710652.jpg'
