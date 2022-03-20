2e9878852b50f0ddad7339acd2a96f213b6a093d
MichaelDepp <michaeldepp7@gmail.com>
2022-03-21 00:23:39 +0800

    fix: add the max width to make the app, width look good on wide screens

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const Container = ({ children, fullScreen, id }) => {
     return (
-        <div
-            className={`px-8 bg-theme-lightBg dark:bg-theme-bg ${
-                fullScreen ? 'h-screen' : ''
-            } w-full`}
-            id={id}>
-            {children}
+        <div className="px-8 lg:px-0 flex justify-center bg-theme-lightBg dark:bg-theme-bg" id={id}>
+            <div className={`max-w-screen-xl ${fullScreen ? 'h-screen' : ''} w-full`}>
+                {children}
+            </div>
         </div>
     );
 };
