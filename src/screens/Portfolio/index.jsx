3e0fed3d71f6a7bb21af83886e18dbf49ae54e4e
MichaelDepp <michaeldepp7@gmail.com>
2022-03-20 12:36:31 +0800

    fix: useEffect to fetch the all data on load

--- src/screens/Portfolio/index.jsx
+++ src/screens/Portfolio/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from '../../components/Container';
 import HeaderTitle from '../../components/HeaderTitle';
 import PortfolioCard from '../../components/PortfolioCard';
@@ -7,6 +7,10 @@ import Chip from 'components/Chip';
 const Portfolio = () => {
     const [activeChip, setActiveChip] = useState('All');
     const [displayData, setDisplayData] = useState([]);
+
+    useEffect(() => {
+        setDisplayData([...portfolioData.flutter, ...portfolioData.uiDesign]);
+    }, []);
 
     const portfolioData = {
         flutter: [
