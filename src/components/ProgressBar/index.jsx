c0560534cfc8ddcd86b68920c2a7213899797eb5
MichaelDepp <michaeldepp7@gmail.com>
2022-03-21 14:30:16 +0800

    fix: change the text size

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -5,8 +5,8 @@ const ProgressBar = ({ skillName, percentage }) => {
         return null;
     }
     return (
-        <div className="pt-4 w-full lg:w-1/2">
-            <p className="font-regular text-md">{skillName}</p>
+        <div className="pt-4 w-full">
+            <p className="font-regular text-base">{skillName}</p>
             <div className="w-full bg-theme-bg/20 dark:bg-theme-lightBg/20 rounded-lg h-1 overflow-hidden">
                 <div
                     className="bg-theme-primary rounded-lg h-1"
