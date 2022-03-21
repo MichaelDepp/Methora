ac8650e0049faa841c37a6043a331dcb18fb761c
MichaelDepp <michaeldepp7@gmail.com>
2022-03-21 14:29:59 +0800

    fix: refactor the title to be relative and centerred

--- src/components/HeaderTitle/index.jsx
+++ src/components/HeaderTitle/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const HeaderTitle = ({ title }) => {
     return (
-        <div className="relative">
+        <div className="relative text-center">
             <h1 className="absolute right-0 left-0 top-2.5 font-bold text-3xl text-theme-primary text-center z-10">
                 {title}
             </h1>
-            <span className="absolute inset-0 text-6xl font-bold leading-none text-center text-theme-bg/5 dark:text-theme-lightBg/5">
+            <span className="text-6xl font-bold leading-none text-theme-bg/5 dark:text-theme-lightBg/5">
                 {title}
             </span>
         </div>
