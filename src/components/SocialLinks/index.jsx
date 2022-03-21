39ef419cca6567659d45d41b21b6632c7fd4c82e
MichaelDepp <michaeldepp7@gmail.com>
2022-03-21 18:55:31 +0800

    refactor: make the sociaL links reponsive n good colors

--- src/components/SocialLinks/index.jsx
+++ src/components/SocialLinks/index.jsx
@@ -5,14 +5,20 @@ import {
     AiOutlineGithub,
     AiFillLinkedin
 } from 'react-icons/ai';
+import { useTheme } from 'next-themes';
+import constants from '../../constants';
 
 const SocialLink = () => {
+    const { theme } = useTheme();
+    const colorConfig =
+        theme === 'dark' ? constants.colors.theme.lightBg : constants.colors.theme.dark;
+
     return (
-        <div className="flex flex-row justify-between my-8">
-            <AiOutlineTwitter size={18} />
-            <AiOutlineGithub size={18} />
-            <AiFillLinkedin size={18} />
-            <AiOutlineDribbble size={18} />
+        <div className="flex flex-row justify-between my-8 mx-16 lg:mx-0 lg:mr-32">
+            <AiOutlineTwitter size={18} color={colorConfig} />
+            <AiOutlineGithub size={18} color={colorConfig} />
+            <AiFillLinkedin size={18} color={colorConfig} />
+            <AiOutlineDribbble size={18} color={colorConfig} />
         </div>
     );
 };
