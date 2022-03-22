394be8e0cde7a69e51b86ae0e18bdba820aca7e5
MichaelDepp <michaeldepp7@gmail.com>
2022-03-22 13:35:17 +0800

    fix: the prop error

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Button = ({ text, onClick }) => {
-    if (!text) {
+const Button = (props) => {
+    if (!props.text) {
         return null;
     }
     return (
         <button
-            onClick={onClick}
+            onClick={props.onClick}
             className="text-white dark:text-theme-bg bg-theme-primary focus:ring-4 font-bold rounded-lg text-lg px-4 py-1 lg:py-2 text-center mb-2">
-            {text}
+            {props.text}
         </button>
     );
 };
