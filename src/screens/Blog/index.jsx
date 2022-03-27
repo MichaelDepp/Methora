67ede6e12afb055a5996fb598791a70c3f9a955e
MichaelDepp <michaeldepp7@gmail.com>
2022-03-27 19:39:11 +0800

    refactor: add the framer animation for the blog cards

--- src/screens/Blog/index.jsx
+++ src/screens/Blog/index.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAnimation, motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import BlogCard from 'components/BlogCard';
 import Container from 'components/Container';
 import HeaderTitle from 'components/HeaderTitle';
 
 const Blog = () => {
+    const { ref, inView } = useInView();
+    const animation = useAnimation();
+
+    useEffect(() => {
+        if (inView) {
+            animation.start({
+                transition: { duration: 0.5, delay: 0.5, ease: 'easeIn' },
+                opacity: 1,
+                x: 0
+            });
+        } else {
+            animation.start({
+                transition: { duration: 0.5, ease: 'easeOut' },
+                opacity: 0,
+                x: '-5%'
+            });
+        }
+    }, [inView]);
+
     return (
         <Container id="blog" fullScreen={false}>
             <div className="pt-8">
                 <HeaderTitle title={'Blog'} />
             </div>
-            <div className="py-8 lg:py-16 justify-items-center grid grid-rows-1 gap-8 lg:grid-cols-3 lg:gap-16">
+            <motion.div
+                ref={ref}
+                animate={animation}
+                className="py-8 lg:py-16 justify-items-center grid grid-rows-1 gap-8 lg:grid-cols-3 lg:gap-16">
                 <BlogCard />
                 <BlogCard />
                 <BlogCard />
                 <BlogCard />
-            </div>
+            </motion.div>
         </Container>
     );
 };
