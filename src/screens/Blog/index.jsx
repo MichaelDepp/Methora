c30ab84cb91422f4c6d6a4358baf6bc8dc7025ef
MichaelDepp <michaeldepp7@gmail.com>
2022-03-29 00:16:18 +0800

    refactor: handle the blog rendering changes in the blog screen

--- src/screens/Blog/index.jsx
+++ src/screens/Blog/index.jsx
@@ -25,6 +25,37 @@ const Blog = () => {
         }
     }, [inView]);
 
+    const blogData = [
+        {
+            title: 'How many programming languages do you regularly switch between?',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur, mauris eget rutrum maximus, enim dui interdum',
+            imageSrc:
+                'https://images.pexels.com/photos/5496463/pexels-photo-5496463.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        },
+        {
+            title: 'How to Be a 10x Software Engineer',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur, mauris eget rutrum maximus, enim dui interdum',
+            imageSrc:
+                'https://images.pexels.com/photos/1181288/pexels-photo-1181288.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        },
+        {
+            title: '💪Become a Front-end Master using these awesome tools 🖱',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur, mauris eget rutrum maximus, enim dui interdum',
+            imageSrc:
+                'https://images.pexels.com/photos/1092644/pexels-photo-1092644.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        },
+        {
+            title: '4 Core things to always do when cleaning your data for predictive models.',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur, mauris eget rutrum maximus, enim dui interdum',
+            imageSrc:
+                'https://images.pexels.com/photos/5952738/pexels-photo-5952738.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        }
+    ];
+
     return (
         <Container id="blog" fullScreen={false}>
             <div className="pt-8">
@@ -34,10 +65,14 @@ const Blog = () => {
                 ref={ref}
                 animate={animation}
                 className="py-8 lg:py-16 justify-items-center grid grid-rows-1 gap-8 lg:grid-cols-3 lg:gap-16">
-                <BlogCard />
-                <BlogCard />
-                <BlogCard />
-                <BlogCard />
+                {blogData.map((blog, key) => (
+                    <BlogCard
+                        key={key}
+                        title={blog.title}
+                        description={blog.description}
+                        imageSrc={blog.imageSrc}
+                    />
+                ))}
             </motion.div>
         </Container>
     );
