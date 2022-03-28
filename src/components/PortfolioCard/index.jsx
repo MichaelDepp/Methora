ae122ee9702c560725105944afe1e7a69a40be98
MichaelDepp <michaeldepp7@gmail.com>
2022-03-29 00:17:10 +0800

    refactor: add the tile text and handle the animation

--- src/components/PortfolioCard/index.jsx
+++ src/components/PortfolioCard/index.jsx
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-const PortfolioCard = ({ imageSrc, onClick }) => {
+const PortfolioCard = ({ imageSrc, onClick, title }) => {
     const [isHovered, setHovered] = useState(false);
     return (
         <a
             onClick={onClick}
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}>
-            <div className="absolute hover:bg-theme-primary/50 w-64 duration-300 h-80 lg:w-80 lg:h-112 rounded-lg grid content-end">
+            <div className="absolute hover:bg-theme-primary/80 w-64 duration-300 h-80 lg:w-80 lg:h-112 rounded-lg grid content-center cursor-pointer">
                 <h3
-                    className={`font-bold text-5xl text-theme-bg leading-none break-all p-4 transition-opacity duration-150 ease-in-out ${
+                    className={`font-bold text-2xl text-theme-bg text-center leading-none break-all p-4 transition-opacity duration-150 ease-in-out ${
                         isHovered ? 'opacity-100' : 'opacity-0'
                     }`}>
-                    {'Facial Recognition Mobile Application'}
+                    {title}
                 </h3>
             </div>
             <img
                 className="object-cover w-64 h-80 lg:w-80 lg:h-112 rounded-lg"
                 src={imageSrc}
-                alt="Portfolio"
+                alt={title}
             />
         </a>
     );
