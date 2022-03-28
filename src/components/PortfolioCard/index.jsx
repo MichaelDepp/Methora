0b900f861e26309e638428646b02b27c63c74623
MichaelDepp <michaeldepp7@gmail.com>
2022-03-28 16:34:38 +0800

    fix: handle the portfolio card and transition

--- src/components/PortfolioCard/index.jsx
+++ src/components/PortfolioCard/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const PortfolioCard = ({ imageSrc, onClick }) => {
+    const [isHovered, setHovered] = useState(false);
     return (
-        <a onClick={onClick}>
-            <div className="absolute hover:bg-theme-primary/50 w-64 duration-300 h-80 lg:w-80 lg:h-112 rounded-lg" />
+        <a
+            onClick={onClick}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}>
+            <div className="absolute hover:bg-theme-primary/50 w-64 duration-300 h-80 lg:w-80 lg:h-112 rounded-lg grid content-end">
+                <h3
+                    className={`font-bold text-5xl text-theme-bg leading-none break-all p-4 transition-opacity duration-150 ease-in-out ${
+                        isHovered ? 'opacity-100' : 'opacity-0'
+                    }`}>
+                    {'Facial Recognition Mobile Application'}
+                </h3>
+            </div>
             <img
                 className="object-cover w-64 h-80 lg:w-80 lg:h-112 rounded-lg"
                 src={imageSrc}
