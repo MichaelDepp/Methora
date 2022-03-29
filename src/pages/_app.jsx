f3665bbbba5cbd85283596bfc7510c6540aeb233
MichaelDepp <michaeldepp7@gmail.com>
2022-03-30 00:31:37 +0800

    feat: install react slick

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,9 @@ import '../styles/global.css';
 import { ThemeProvider } from 'next-themes';
 import Head from 'next/head';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 function MyApp({ Component, pageProps }) {
     return (
         <>
