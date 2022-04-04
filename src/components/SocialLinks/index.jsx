732bf2e60c3faf728077e7136fe5352d0b6f6ecd
MichaelDepp <michaeldepp7@gmail.com>
2022-04-04 14:56:51 +0800

    refactor: the classname styles for the icons and make the icons link clickable

--- src/components/SocialLinks/index.jsx
+++ src/components/SocialLinks/index.jsx
@@ -1,31 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
     AiOutlineTwitter,
     AiOutlineDribbble,
     AiOutlineGithub,
     AiFillLinkedin
 } from 'react-icons/ai';
-import { useTheme } from 'next-themes';
-import constants from '../../constants';
 
 const SocialLink = () => {
-    const [iconColor, setIconColor] = useState(constants.colors.theme.lightBg);
-    const { theme } = useTheme();
+    const iconStyle =
+        'cursor-pointer text-theme-dark dark:text-theme-lightBg hover:text-theme-primary lg:hover:text-theme-primary';
 
-    useEffect(() => {
-        if (theme === 'light') {
-            setIconColor(constants.colors.theme.dark);
-        } else {
-            setIconColor(constants.colors.theme.lightBg);
-        }
-    }, [theme]);
+    const onClickIcon = (link) => window.open(link);
 
     return (
         <div className="flex flex-row justify-between my-8 mx-16 lg:mx-0 lg:mr-32">
-            <AiOutlineTwitter size={18} color={iconColor} />
-            <AiOutlineGithub size={18} color={iconColor} />
-            <AiFillLinkedin size={18} color={iconColor} />
-            <AiOutlineDribbble size={18} color={iconColor} />
+            <AiOutlineTwitter
+                size={18}
+                className={iconStyle}
+                onClick={() => onClickIcon('https://twitter.com/NASA')}
+            />
+            <AiOutlineGithub
+                size={18}
+                className={iconStyle}
+                onClick={() => onClickIcon('https://twitter.com/NASA')}
+            />
+            <AiFillLinkedin
+                size={18}
+                className={iconStyle}
+                onClick={() => onClickIcon('https://twitter.com/NASA')}
+            />
+            <AiOutlineDribbble
+                size={18}
+                className={iconStyle}
+                onClick={() => onClickIcon('https://twitter.com/NASA')}
+            />
         </div>
     );
 };
