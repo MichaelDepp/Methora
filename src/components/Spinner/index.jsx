4d5f51df729bc2334848b4adcfd001d9a0212809
MichaelDepp <michaeldepp7@gmail.com>
2022-04-05 10:36:03 +0800

    feat: create the spinner component

---
+++ src/components/Spinner/index.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const Spinner = (props) => {
+    const cubeStyle = `w-4 h-4 ${props.color}`;
+
+    const scaleConfig = {
+        scale: [1, 0, 1, 1]
+    };
+
+    const cubeTransitionConfig = { repeat: Infinity, ease: 'easeInOut', duration: 1.3 };
+
+    return (
+        <div className="absolute inset-0 m-auto w-12 h-12 grid grid-cols-3 grid-rows-3">
+            <motion.div
+                className={cubeStyle}
+                animate={scaleConfig}
+                transition={{
+                    delay: 0.2,
+                    ...cubeTransitionConfig
+                }}></motion.div>
+            <motion.div
+                className={cubeStyle}
+                animate={scaleConfig}
+                transition={{
+                    delay: 0.3,
+                    ...cubeTransitionConfig
+                }}></motion.div>
+            <motion.div
+                className={cubeStyle}
+                animate={scaleConfig}
+                transition={{
+                    delay: 0.4,
+                    ...cubeTransitionConfig
+                }}></motion.div>
+            <motion.div
+                className={cubeStyle}
+                animate={scaleConfig}
+                transition={{
+                    delay: 0.1,
+                    ...cubeTransitionConfig
+                }}></motion.div>
+            <motion.div
+                className={cubeStyle}
+                animate={scaleConfig}
+                transition={{
+                    delay: 0.2,
+                    ...cubeTransitionConfig
+                }}></motion.div>
+            <motion.div
+                className={cubeStyle}
+                animate={scaleConfig}
+                transition={{
+                    delay: 0.3,
+                    ...cubeTransitionConfig
+                }}></motion.div>
+            <motion.div
+                className={cubeStyle}
+                animate={scaleConfig}
+                transition={{
+                    delay: 0,
+                    ...cubeTransitionConfig
+                }}></motion.div>
+            <motion.div
+                className={cubeStyle}
+                animate={scaleConfig}
+                transition={{
+                    delay: 0.1,
+                    ...cubeTransitionConfig
+                }}></motion.div>
+            <motion.div
+                className={cubeStyle}
+                animate={scaleConfig}
+                transition={{
+                    delay: 0.2,
+                    ...cubeTransitionConfig
+                }}></motion.div>
+        </div>
+    );
+};
+
+export default Spinner;
