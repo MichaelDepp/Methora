486b32c5ff6afbcae9d0cdf9ff565acd266da240
MichaelDepp <michaeldepp7@gmail.com>
2022-04-06 01:19:25 +0800

    refactor: add the header bar for lg screens and handle its animation

--- src/components/HeaderBar/index.jsx
+++ src/components/HeaderBar/index.jsx
@@ -1,30 +1,79 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
+import Link from 'next/link';
 import { useTheme } from 'next-themes';
 import Hamburger from 'hamburger-react';
 import { RiMoonFill, RiSunFill } from 'react-icons/ri';
+import upperFirst from 'lodash/upperFirst';
+import { useAnimation, motion } from 'framer-motion';
+
 import Drawer from '../Drawer';
 import constants from '../../constants';
-
 import LinkPage from '../LinkPage';
 
 const HeaderBar = () => {
     const [isOpen, setOpen] = useState(false);
+    const [currentPage, setCurrentPage] = useState('home');
+    const [barHover, setBarHover] = useState(false);
     const { theme, setTheme } = useTheme();
+    const animation = useAnimation();
+
+    useEffect(() => {
+        if (currentPage === 'home') {
+            animation.start({
+                y: 0,
+                transition: { duration: 0.5, ease: 'easeIn' }
+            });
+        } else if (currentPage !== 'home' && barHover) {
+            animation.start({
+                y: 0,
+                transition: { duration: 0.5, ease: 'easeIn' }
+            });
+        } else {
+            animation.start({
+                y: -100,
+                transition: { duration: 0.5, ease: 'easeOut' }
+            });
+        }
+    }, [barHover, currentPage]);
 
     const onSwitchTheme = (mode) => setTheme(mode);
 
     const closeBar = () => setOpen(false);
 
+    const pages = ['home', 'skills', 'portfolio', 'testimony', 'blog', 'contact'];
+
     return (
         <>
-            <div className="fixed z-50 top-0 w-full flex items-center justify-between flex-wrap">
-                <Hamburger
-                    color={constants.colors.theme.primary}
-                    size={20}
-                    toggled={isOpen}
-                    toggle={setOpen}
-                />
+            <div
+                onMouseEnter={() => setBarHover(true)}
+                onMouseLeave={() => setBarHover(false)}
+                className="fixed z-50 top-0 w-full flex items-center justify-between flex-wrap lg:bg-theme-lightBg/90 dark:lg:bg-theme-bg/90">
+                <div className="lg:invisible">
+                    <Hamburger
+                        color={constants.colors.theme.primary}
+                        size={20}
+                        toggled={isOpen}
+                        toggle={setOpen}
+                    />
+                </div>
+                <motion.div animate={animation} className="grid-flow-col gap-16 hidden lg:grid">
+                    {pages.map((page, key) => {
+                        const colorCofig =
+                            currentPage === page
+                                ? 'text-theme-primary'
+                                : 'text-theme-bg dark:text-theme-lightBg';
+                        return (
+                            <Link key={key} href={`#${page.toLowerCase()}`}>
+                                <p
+                                    onClick={() => setCurrentPage(page)}
+                                    className={`${colorCofig} font-bold text-lg hover:text-theme-primary dark:hover:text-theme-primary cursor-pointer`}>
+                                    {upperFirst(page)}
+                                </p>
+                            </Link>
+                        );
+                    })}
+                </motion.div>
                 <div className="p-3">
                     {theme === 'light' ? (
                         <RiMoonFill
@@ -43,12 +92,9 @@ const HeaderBar = () => {
             </div>
             <Drawer isOpen={isOpen} setIsOpen={setOpen}>
                 <div className="grid grid-flow-row gap-4">
-                    <LinkPage page={'Home'} onClick={closeBar} />
-                    <LinkPage page={'Skills'} onClick={closeBar} />
-                    <LinkPage page={'Portfolio'} onClick={closeBar} />
-                    <LinkPage page={'Testimony'} onClick={closeBar} />
-                    <LinkPage page={'Blog'} onClick={closeBar} />
-                    <LinkPage page={'Contact'} onClick={closeBar} />
+                    {pages.map((page, key) => (
+                        <LinkPage key={key} page={upperFirst(page)} onClick={closeBar} />
+                    ))}
                 </div>
             </Drawer>
         </>
