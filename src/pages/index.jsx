4846d5f00b4b159e97c6f18d561c3fa01278cc1e
MichaelDepp <michaeldepp7@gmail.com>
2022-04-05 10:36:28 +0800

    refactor: add the spinner to the index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
+
 import Blog from 'screens/Blog';
 import Contact from 'screens/Contact';
 import Home from 'screens/Home';
@@ -7,23 +8,38 @@ import Skills from 'screens/Skills';
 import Testimony from 'screens/Testimony';
 import Portfolio from 'screens/Portfolio';
 import HeaderBar from 'components/HeaderBar';
+import Spinner from 'components/Spinner';
 import Footer from 'components/Footer';
 
-const Index = () => (
-    <>
-        <Head>
-            <title>Methora Portfolio Theme - Confetti Themes</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Home />
-        <Skills />
-        <Portfolio />
-        <Testimony />
-        <Blog />
-        <Contact />
-        <HeaderBar />
-        {/* <Footer /> */}
-    </>
-);
+const Index = () => {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(true), 2000);
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <title>Methora Portfolio Theme - Confetti Themes</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {!loading ? (
+                <Spinner color="bg-theme-primary" />
+            ) : (
+                <>
+                    <Home />
+                    <Skills />
+                    <Portfolio />
+                    <Testimony />
+                    <Blog />
+                    <Contact />
+                    <HeaderBar />
+                    {/* <Footer /> */}
+                </>
+            )}
+        </>
+    );
+};
 
 export default Index;
