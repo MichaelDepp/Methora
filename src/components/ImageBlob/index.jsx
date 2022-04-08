8e6683f5cb035cdaf925a50564200d43e947458d
MichaelDepp <michaeldepp7@gmail.com>
2022-04-08 11:00:39 +0800

    refactor: remove comments

--- src/components/ImageBlob/index.jsx
+++ src/components/ImageBlob/index.jsx
@@ -6,7 +6,6 @@ const ImageBlob = (props) => {
             <img
                 className="h-64 w-64 rounded-full object-contain lg:h-96 lg:w-96"
                 src={props.image}
-                // src="https://randomuser.me/api/portraits/women/81.jpg"
                 alt="user image"
             />
         </div>
