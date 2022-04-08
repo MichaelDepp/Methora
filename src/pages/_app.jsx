5e048e86b152ead4e2b640898905b4c39b42268b
MichaelDepp <michaeldepp7@gmail.com>
2022-04-08 17:42:54 +0800

    refactor: rmake document and app as arrow function

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import '../styles/global.css';
 import { ThemeProvider } from 'next-themes';
 
+// Include the CSS for react-slick slider
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
     return (
+        // ThemeProvider will be used for dark mode light mode configurations
+        // It also stores the user's prefered mode in local storage
         <ThemeProvider attribute="class">
             <Component {...pageProps} />
         </ThemeProvider>
     );
-}
+};
 
 export default MyApp;
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default class Document extends NextDocument {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link rel="shortcut icon" href="./assets/rocket-icon.png" />
-                    <link rel="apple-touch-icon" href="./assets/rocket-icon.png" />
-                    <link rel="manifest" href="/manifest.json" />
-                    <meta name="description" content="Methora Theme" />
-                </Head>
-                <body className="font-display">
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
-}
+const Document = () => {
+    return (
+        <Html>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="shortcut icon" href="./assets/rocket-icon.png" />
+                <link rel="apple-touch-icon" href="./assets/rocket-icon.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="description" content="Methora Theme" />
+            </Head>
+            <body className="font-display">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
+
+export default Document;
