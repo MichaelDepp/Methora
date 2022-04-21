9e2e32d9a681c27e004e2920af82b519e8c956ff
MichaelDepp <michaeldepp7@gmail.com>
2022-04-21 17:15:50 +0800

    fix: handle the image blob sizing for small screens

--- src/components/ImageBlob/index.jsx
+++ src/components/ImageBlob/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ImageBlob = (props) => {
     return (
-        <div className="h-64 w-64 animate-blob rounded-full bg-theme-primary shadow-2xl lg:h-96 lg:w-96">
+        <div className="h-56 w-56 animate-blob rounded-full bg-theme-primary shadow-2xl xs:h-64 xs:w-64 lg:h-96 lg:w-96">
             <img
-                className="h-64 w-64 rounded-full object-contain lg:h-96 lg:w-96"
+                className="h-56 w-56 rounded-full object-contain xs:h-64 xs:w-64 lg:h-96 lg:w-96"
                 src={props.image}
                 alt="user image"
             />
